99af1e3ad4f23d7d164587c33b52ef8ad28fdfe6
e.milkheev <milheev.e@gmail.com>
2022-06-20 17:35:44 +0300

    fix endpoint user

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,5 +1,5 @@
 from rest_framework import permissions
-
+from users.models import CustomUser
 
 # class IsAuthorOrModeratorPermission(permissions.BasePermission):
 
@@ -12,14 +12,54 @@ from rest_framework import permissions
 #         )
 
 
-class IsAuthorOrStaffOrReadOnly(permissions.BasePermission):
+# class IsAuthorOrStaffOrReadOnly(permissions.BasePermission):
+#
+#     def has_object_permission(self, request, view, obj):
+#         if request.method in permissions.SAFE_METHODS:
+#             return True
+#         elif request.user.is_anonymous:
+#             return False
+#         else:
+#             return (request.user.is_admin
+#                     or request.user.is_moderator
+#                     or obj.author == request.user)
 
-    def has_object_permission(self, request, view, obj):
+class IsAuthorOrStaffOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        elif request.user.is_anonymous:
+        if request.user.is_authenticated:
+            return bool(request.user.is_staff or request.user.role == 'admin')
+
+
+class IsUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            if request.user.role in CustomUser.USER:
+                return True
+        return False
+
+
+class IsModerator(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            if request.user.role in CustomUser.MODERATOR:
+                return True
+        return False
+
+
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            if request.user.role in CustomUser.ADMIN:
+                return True
             return False
+
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_authenticated:
+            return obj.author == request.user
         else:
-            return (request.user.is_admin
-                    or request.user.is_moderator
-                    or obj.author == request.user)
+            return False
+
